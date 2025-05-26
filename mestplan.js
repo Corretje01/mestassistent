5bcaecd200c97723e6ac37efbc87ca251ff67f38
Corretje01 <96739694+Corretje01@users.noreply.github.com>
2025-05-26 19:55:59 +0200

    Update mestplan.js

--- mestplan.js
+++ mestplan.js
@@ -56,6 +56,7 @@ const standaardSliders = [
   { id: 'fosfaat',   max: totaalC,  unit: 'kg' },
   { id: 'kalium',    max: 7500, unit: 'kg' },
   { id: 'organisch', max: 3000, unit: 'kg' }
+  { id: 'kunststikstof', max: 1000,   unit: 'kg' }
 ];
 standaardSliders.forEach(({id, max, unit}) => initSlider(id, max, unit));
 
