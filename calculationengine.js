03e90f8038147a621995256efd8edde330372d77
Corretje01 <96739694+Corretje01@users.noreply.github.com>
2025-06-25 14:55:12 +0200

    Update calculationengine.js

--- calculationengine.js
+++ calculationengine.js
@@ -1,38 +1,32 @@
 /**
  * calculationengine.js
- * Pure rekenlogica voor mestplan applicatie
+ * Rekenlogica voor nutrientenberekening
  */
 
 import { StateManager } from './statemanager.js';
 
 export const CalculationEngine = (() => {
 
-  function calculateTotalNutrients(inclusiefKunstmest = false) {
+  function berekenNutriënten(inclusiefKunstmest = false) {
     const actieveMest = StateManager.getActieveMest();
+    let totaal = { stikstof: 0, fosfaat: 0, kalium: 0, organisch: 0 };
 
-    const totals = { N: 0, P: 0, K: 0, OS: 0, DS: 0, BG: 0, FIN: 0 };
-
-    for (const key in actieveMest) {
-      const mest = actieveMest[key];
-      const ton = mest.ton || 0;
-      totals.N  += ton * (mest.N_kg_per_ton || 0);
-      totals.P  += ton * (mest.P_kg_per_ton || 0);
-      totals.K  += ton * (mest.K_kg_per_ton || 0);
-      totals.OS += ton * ((mest.OS_percent || 0) / 100);
-      totals.DS += ton * ((mest.DS_percent || 0) / 100);
-      totals.BG += ton * (mest.biogaspotentieel_m3_per_ton || 0);
-      totals.FIN+= ton * ((mest.Inkoopprijs_per_ton || 0) + 10);
+    for (const mest of Object.values(actieveMest)) {
+      totaal.stikstof  += mest.ton * (mest.N_kg_per_ton || 0);
+      totaal.fosfaat   += mest.ton * (mest.P_kg_per_ton || 0);
+      totaal.kalium    += mest.ton * (mest.K_kg_per_ton || 0);
+      totaal.organisch += mest.ton * ((mest.OS_percent || 0) / 100);
     }
 
     if (inclusiefKunstmest) {
-      totals.N += StateManager.getKunstmest();
+      totaal.stikstof += StateManager.getKunstmest();
     }
 
-    return totals;
+    return totaal;
   }
 
   return {
-    calculateTotalNutrients
-  }
+    berekenNutriënten
+  };
 
 })();
