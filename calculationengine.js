ae0cc5d7a3eac7d288ee918ddb99d3abd8101b95
Corretje01 <96739694+Corretje01@users.noreply.github.com>
2025-06-25 15:18:41 +0200

    Update calculationengine.js

--- calculationengine.js
+++ calculationengine.js
@@ -1,6 +1,6 @@
 /**
  * calculationengine.js
- * Rekenlogica voor nutrientenberekening
+ * Rekenlogica voor nutrienten en financieel
  */
 
 import { StateManager } from './statemanager.js';
@@ -9,13 +9,14 @@ export const CalculationEngine = (() => {
 
   function berekenNutriënten(inclusiefKunstmest = false) {
     const actieveMest = StateManager.getActieveMest();
-    let totaal = { stikstof: 0, fosfaat: 0, kalium: 0, organisch: 0 };
+    let totaal = { stikstof: 0, fosfaat: 0, kalium: 0, organisch: 0, financieel: 0 };
 
     for (const mest of Object.values(actieveMest)) {
       totaal.stikstof  += mest.ton * (mest.N_kg_per_ton || 0);
       totaal.fosfaat   += mest.ton * (mest.P_kg_per_ton || 0);
       totaal.kalium    += mest.ton * (mest.K_kg_per_ton || 0);
       totaal.organisch += mest.ton * ((mest.OS_percent || 0) / 100);
+      totaal.financieel += mest.ton * ((mest.Inkoopprijs_per_ton || 0) + 10); // inclusief transportkosten
     }
 
     if (inclusiefKunstmest) {
