ef8b89136a3852076fae43ee22a3423211f12154
Corretje01 <96739694+Corretje01@users.noreply.github.com>
2025-08-07 10:45:36 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // 2) Jouw project-gegevens
 const SUPABASE_URL  = 'https://joxzxtdkjenyayddtwmn.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9…';  // je anon/public key
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpveHp4dGRramVueWF5ZGR0d21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwOTI4NTEsImV4cCI6MjA2ODY2ODg1MX0.4gHFI3jPjiVxLVTlgOsvhoa-i6XDkzcQP22FTPcaHm4';  // je anon/public key
 
 // 3) Één instantie, overal hergebruiken
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
