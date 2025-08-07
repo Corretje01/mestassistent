b4a6de7dcb2d210cb28abcf2c3cdcfcb0af29c01
Corretje01 <96739694+Corretje01@users.noreply.github.com>
2025-08-07 10:36:19 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL   = 'https://joxzxtdkjenyayddtwmn.supabase.co';  // jouw projecten-URL
 const SUPABASE_ANON  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpveHp4dGRramVueWF5ZGR0d21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwOTI4NTEsImV4cCI6MjA2ODY2ODg1MX0.4gHFI3jPjiVxLVTlgOsvhoa-i6XDkzcQP22FTPcaHm4';     // jouw public anon key
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
